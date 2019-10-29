f880ee062cf8b0c8f8157d58dd0bf50c69d6ed27
Arthur PREVOST <artprevo@e1r8p8.42.fr>
2019-10-29 16:36:07 +0100

    nettoyage

--- checker.sh
+++
@@ -1,7 +0,0 @@
-make
-mv artprevo.filler ../../checker/filler_check/players/artprevo.filler
-cd ..
-cd ..
-cd checker
-cd filler_check
-sh correction.sh -b players/artprevo.filler -r 
--- test.sh
+++
@@ -1,5 +0,0 @@
-make
-rm data.txt
-cp artprevo.filler test/players
-cd test
-./filler_vm -f maps/map02 -p1 players/artprevo.filler -p2 players/superjeannot.filler
